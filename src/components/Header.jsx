7025f4e619109718bbdc67ef726e425edf94bff1
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2024-12-01 14:56:40 -0600

    added code to Header and Navbar components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,32 @@
 // src/components/Header.js
-import React from "react";
+import React from 'react';
+import Navbar from 'Navbar';
+const Header = () => {
+ return (
+  <header>
+    <h1>Header</h1>
+    <Navbar />
+  </header>
+ );
+};
 
-function Header() {
-  const handleNavigation = (sectionId) => {
-    document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
-  };
+// function Header() {
+//   const handleNavigation = (sectionId) => {
+//     document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+//   };
 
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li onClick={() => handleNavigation("hero")}>Home</li>
-          <li onClick={() => handleNavigation("projects")}>Projects</li>
-          {/* <li onClick={() => handleNavigation("skills")}>Skills</li> */}
-          <li onClick={() => handleNavigation("contact")}>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+//   return (
+//     <header>
+//       <nav>
+//         <ul>
+//           <li onClick={() => handleNavigation("hero")}>Home</li>
+//           <li onClick={() => handleNavigation("projects")}>Projects</li>
+//           {/* <li onClick={() => handleNavigation("skills")}>Skills</li> */}
+//           <li onClick={() => handleNavigation("contact")}>Contact</li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
 
 export default Header;
