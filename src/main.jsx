d498c092a340685047b47dafbf5fe2c0934c0b83
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2024-12-03 16:52:18 -0600

    remove bootstrap remnants

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
 import App from './App';
 import Hero from './components/navbar/Hero';
