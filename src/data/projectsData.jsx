33c7d2e6f3ae8f77ad82e1a097ad5754c75a06b4
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2024-12-03 19:51:24 -0600

    CSS stuff

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -1,4 +1,3 @@
-
 const projects = [
   {
     title: "Pre-Work Study Guide",
@@ -10,33 +9,33 @@ const projects = [
   {
     title: "README Generator",
     img: "https://placehold.co/200",
-    desc: "",
+    desc: "A CLI tool to generate professional README files.",
     pageLink: "https://k3strl.github.io/prework-study-guide/",
     gitLink: "https://github.com/k3strl/CLI-README.md-generator",
   },
   {
-    title: "Pre-Work Study Guide",
+    title: "Weather Dashboard",
+    img: "https://placehold.co/200",
+    desc: "A web app to check the weather in various cities.",
+    pageLink: "https://k3strl.github.io/weather-dashboard/",
+    gitLink: "https://github.com/k3strl/weather-dashboard",
+  },
+  {
+    title: "Employee Manager",
+    img: "https://placehold.co/200",
+    desc: "A simple task management app to keep track of your to-dos.",
+    pageLink: "https://k3strl.github.io/task-manager/",
+    gitLink: "https://github.com/k3strl/task-manager",
+  },
+  {
+    title: "Vehicle Builder",
     img: "https://placehold.co/200",
     desc: "Description for project 1",
     pageLink: "https://k3strl.github.io/prework-study-guide/",
     gitLink: "https://github.com/k3strl/prework-study-guide",
   },
   {
-    title: "Pre-Work Study Guide",
-    img: "https://placehold.co/200",
-    desc: "Description for project 1",
-    pageLink: "https://k3strl.github.io/prework-study-guide/",
-    gitLink: "https://github.com/k3strl/prework-study-guide",
-  },
-  {
-    title: "Pre-Work Study Guide",
-    img: "https://placehold.co/200",
-    desc: "Description for project 1",
-    pageLink: "https://k3strl.github.io/prework-study-guide/",
-    gitLink: "https://github.com/k3strl/prework-study-guide",
-  },
-  {
-    title: "Pre-Work Study Guide",
+    title: "Project 6",
     img: "https://placehold.co/200",
     desc: "Description for project 1",
     pageLink: "https://k3strl.github.io/prework-study-guide/",
